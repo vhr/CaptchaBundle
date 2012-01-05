7584429463665894a429c53c21ecde6902d717e6
Valentin Hristov <v.hristov@mail.ru>
2012-01-05 20:00:17 +0200

    Set in options list font_size

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,6 +23,7 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('width')->defaultValue(120)->end()
                 ->scalarNode('height')->defaultValue(40)->end()
                 ->scalarNode('font')->defaultValue(__DIR__.'/../Generator/Font/captcha.ttf')->end()
+                ->scalarNode('font_size')->defaultValue(null)->end()
                 ->scalarNode('keep_value')->defaultValue(true)->end()
                 ->scalarNode('charset')->defaultValue('abcdefhjkmnprstuvwxyz23456789')->end()
                 ->scalarNode('as_file')->defaultValue(false)->end()
